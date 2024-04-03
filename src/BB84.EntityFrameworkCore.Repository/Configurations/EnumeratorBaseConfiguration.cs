cd3b27ae8a1fbeb89737515a03e11e1a952d3b34
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-04-03 16:34:36 +0200

    feat: initial implementation
    changes:
    - base repositories added
    - repository abstractions added
    - base model configurations added

---
+++ src/BB84.EntityFrameworkCore.Repository/Configurations/EnumeratorBaseConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using BB84.EntityFrameworkCore.Models.Abstractions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BB84.EntityFrameworkCore.Repository.Configurations;
+
+/// <summary>
+/// The enumerator base configuration class.
+/// </summary>
+/// <inheritdoc cref="IEntityTypeConfiguration{TEntity}"/>
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, entity type configuration.")]
+public abstract class EnumeratorBaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class, IEnumeratorModel
+{
+  /// <inheritdoc/>
+  public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+  {
+    builder.HasKey(x => x.Id)
+      .IsClustered();
+
+    builder.Property(e => e.Id)
+      .IsRequired()
+      .HasColumnOrder(1);
+
+    builder.Property(e => e.Timestamp)
+      .IsRowVersion()
+      .HasColumnOrder(2);
+
+    builder.Property(e => e.Name)
+      .HasMaxLength(50)
+      .IsRequired()
+      .HasColumnOrder(3);
+
+    builder.Property(e => e.Description)
+      .HasMaxLength(250)
+      .IsRequired(false)
+      .HasColumnOrder(4);
+
+    builder.Property(e => e.IsDeleted)
+      .HasDefaultValue(false)
+      .HasColumnOrder(5);
+
+    builder.HasIndex(e => e.Name)
+      .IsClustered()
+      .IsUnique();
+  }
+}
---
+++ src/BB84.EntityFrameworkCore.Repository/IdentityRepository.cs
@@ -0,0 +1,68 @@
+﻿using BB84.EntityFrameworkCore.Models.Abstractions;
+using BB84.EntityFrameworkCore.Repository.Abstractions;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BB84.EntityFrameworkCore.Repository;
+
+/// <summary>
+/// The identity repository class.
+/// </summary>
+/// <inheritdoc cref="IIdentityRepository{TEntity, TKey}"/>
+public abstract class IdentityRepository<TEntity, TKey>(DbContext dbContext) : GenericRepository<TEntity>(dbContext), IIdentityRepository<TEntity, TKey>
+  where TEntity : class, IIdentityModel<TKey>
+  where TKey : IEquatable<TKey>
+{
+  /// <inheritdoc/>
+  public TEntity? GetById(TKey id, bool ignoreQueryFilters = false, bool trackChanges = false)
+  {
+    IQueryable<TEntity> query = PrepareQuery(
+      expression: x => x.Id.Equals(id),
+      ignoreQueryFilters: ignoreQueryFilters,
+      trackChanges: trackChanges
+      );
+
+    return query.SingleOrDefault();
+  }
+
+  /// <inheritdoc/>
+  public async Task<TEntity?> GetByIdAsync(TKey id, bool ignoreQueryFilters = false, bool trackChanges = false, CancellationToken cancellationToken = default)
+  {
+    IQueryable<TEntity> query = PrepareQuery(
+      expression: x => x.Id.Equals(id),
+      ignoreQueryFilters: ignoreQueryFilters,
+      trackChanges: trackChanges
+      );
+
+    return await query.SingleOrDefaultAsync(cancellationToken);
+  }
+
+  /// <inheritdoc/>
+  public IEnumerable<TEntity> GetByIds(IEnumerable<TKey> ids, bool ignoreQueryFilters = false, bool trackChanges = false)
+  {
+    IQueryable<TEntity> query = PrepareQuery(
+      expression: x => ids.Contains(x.Id),
+      ignoreQueryFilters: ignoreQueryFilters,
+      trackChanges: trackChanges
+      );
+
+    return [.. query];
+  }
+
+  /// <inheritdoc/>
+  public async Task<IEnumerable<TEntity>> GetByIdsAsync(IEnumerable<TKey> ids, bool ignoreQueryFilters = false, bool trackChanges = false, CancellationToken cancellationToken = default)
+  {
+    IQueryable<TEntity> query = PrepareQuery(
+      expression: x => ids.Contains(x.Id),
+      ignoreQueryFilters: ignoreQueryFilters,
+      trackChanges: trackChanges
+      );
+
+    return await query.ToListAsync(cancellationToken);
+  }
+}
+
+/// <inheritdoc/>
+public abstract class IdentityRepository<TEntity>(DbContext dbContext) : IdentityRepository<TEntity, Guid>(dbContext),
+  IIdentityRepository<TEntity> where TEntity : class, IIdentityModel
+{ }
