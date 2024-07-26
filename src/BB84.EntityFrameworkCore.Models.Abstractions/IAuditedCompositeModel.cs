4910d15a9e432f6b1400066f00cbd6df7c61a35f
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-07-26 17:26:56 +0200

    feat: default sql value when identity key guid

--- src/BB84.EntityFrameworkCore.Models.Abstractions/IAuditedCompositeModel.cs
+++ src/BB84.EntityFrameworkCore.Models.Abstractions/IAuditedCompositeModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using BB84.EntityFrameworkCore.Models.Abstractions.Components;
+﻿using BB84.EntityFrameworkCore.Models.Abstractions.Components;
 
 namespace BB84.EntityFrameworkCore.Models.Abstractions;
 
