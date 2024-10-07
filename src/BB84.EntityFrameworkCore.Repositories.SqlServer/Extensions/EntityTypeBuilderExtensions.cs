cac9a5248396e276030473485eefd2344f3c3600
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-07 17:09:32 +0200

    faet: extensions methods added and renamed for more clarity

--- src/BB84.EntityFrameworkCore.Repositories.SqlServer/Extensions/EntityTypeBuilderExtensions.cs
+++ src/BB84.EntityFrameworkCore.Repositories.SqlServer/Extensions/EntityTypeBuilderExtensions.cs
@@ -9,21 +9,20 @@ namespace BB84.EntityFrameworkCore.Repositories.SqlServer.Extensions;
 public static class EntityTypeBuilderExtensions
 {
 	/// <summary>
-	/// Configures the table that the entity type maps to when targeting a relational database and configures a history table for the entity mapped to a temporal table.
+	/// Configures the table that the entity type maps to when targeting a relational database
+	/// and configures a history table for the entity mapped to a temporal table.
 	/// </summary>
-	/// <param name="builder">The entity type builder to work with.</param>
+	/// <param name="builder">The builder for the entity type being configured.</param>
 	/// <param name="tableName">The schema of the table.</param>
 	/// <param name="tableSchema">The name of the table.</param>
 	/// <param name="historySchema">The schema of the history table.</param>
-	/// <returns></returns>
-	public static EntityTypeBuilder ToHistoryTable(this EntityTypeBuilder builder, string? tableName = null, string? tableSchema = null, string historySchema = "history")
+	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
+	public static EntityTypeBuilder ToHistoryTable(this EntityTypeBuilder builder, string? tableName = null, string? tableSchema = "dbo", string historySchema = "history")
 	{
 		tableName ??= builder.Metadata.ClrType.Name;
 
-		return builder.ToTable(tableName, tableSchema,
-			buildAction => buildAction.IsTemporal(
-				buildAction => buildAction.UseHistoryTable(tableName, historySchema)
-				)
-			);
+		return builder.ToTable(tableName, tableSchema, tb
+			=> tb.IsTemporal(ttb
+				=> ttb.UseHistoryTable(tableName, historySchema)));
 	}
 }
--- src/BB84.EntityFrameworkCore.Repositories.SqlServer/Extensions/PropertyBuilderExtensions.cs
+++ src/BB84.EntityFrameworkCore.Repositories.SqlServer/Extensions/PropertyBuilderExtensions.cs
@@ -9,12 +9,33 @@ namespace BB84.EntityFrameworkCore.Repositories.SqlServer.Extensions;
 public static class PropertyBuilderExtensions
 {
 	/// <summary>
+	/// Configures the data type of the column to <b>binary</b> when targeting a relational database.
+	/// </summary>
+	/// <param name="builder">The builder for the property being configured.</param>
+	/// <param name="precision">This can be an integer from 1 to 8000.
+	/// The default fractional scale is 8000.</param>
+	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
+	public static PropertyBuilder IsBinaryColumn(this PropertyBuilder builder, int precision = 8000)
+		=> precision is < 1 or > 8000
+			? throw new ArgumentOutOfRangeException(nameof(precision), "Must be between 1 to 8000.")
+			: builder.HasColumnType($"binary({precision})");
+
+	/// <summary>
 	/// Configures the data type of the column to <b>date</b> when targeting a relational database.
 	/// </summary>
 	/// <param name="builder">The builder for the property being configured.</param>
 	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
-	public static PropertyBuilder HasDateColumnType(this PropertyBuilder builder)
+	public static PropertyBuilder IsDateColumn(this PropertyBuilder builder)
 		=> builder.HasColumnType("date");
+
+	/// <summary>
+	/// Configures the data type of the column to <b>date time</b> when targeting a relational database.
+	/// </summary>
+	/// <param name="builder">The builder for the property being configured.</param>
+	/// <param name="small">Inidactes if <b>small date time</b> should be used.</param>
+	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
+	public static PropertyBuilder IsDateTimeColumn(this PropertyBuilder builder, bool small = false)
+		=> builder.HasColumnType(small ? "smalldatetime" : "datetime");
 
 	/// <summary>
 	/// Configures the data type of the column to <b>money</b> when targeting a relational database.
@@ -22,8 +43,16 @@ public static class PropertyBuilderExtensions
 	/// <param name="builder">The builder for the property being configured.</param>
 	/// <param name="small">Inidactes if <b>small money</b> should be used.</param>
 	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
-	public static PropertyBuilder HasMoneyColumnType(this PropertyBuilder builder, bool small = false)
+	public static PropertyBuilder IsMoneyColumn(this PropertyBuilder builder, bool small = false)
 		=> builder.HasColumnType(small ? "smallmoney" : "money");
+
+	/// <summary>
+	/// Configures the data type of the column to <b>sysname</b> when targeting a relational database.
+	/// </summary>
+	/// <param name="builder">The builder for the property being configured.</param>
+	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
+	public static PropertyBuilder IsSysNameColumn(this PropertyBuilder builder)
+		=> builder.HasColumnType("sysname");
 
 	/// <summary>
 	/// Configures the data type of the column to <b>time</b> when targeting a relational database.
@@ -32,7 +61,7 @@ public static class PropertyBuilderExtensions
 	/// <param name="precision">This can be an integer from 0 to 7.
 	/// The default fractional scale is 7 (100ns).</param>
 	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
-	public static PropertyBuilder HasTimeColumnType(this PropertyBuilder builder, int precision = 7)
+	public static PropertyBuilder IsTimeColumn(this PropertyBuilder builder, int precision = 7)
 		=> precision is < 0 or > 7
 			? throw new ArgumentOutOfRangeException(nameof(precision), "Must be between 0 to 7.")
 			: builder.HasColumnType($"time({precision})");
@@ -41,12 +70,12 @@ public static class PropertyBuilderExtensions
 	/// Configures the data type of the column to <b>varbinary</b> when targeting a relational database.
 	/// </summary>
 	/// <param name="builder">The builder for the property being configured.</param>
-	/// <param name="precision">This can be an integer from 0 to 4000.
+	/// <param name="precision">This can be an integer from 0 to 8000.
 	/// The default fractional scale is 0 (max).</param>
 	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
-	public static PropertyBuilder HasVarbinaryColumnType(this PropertyBuilder builder, int precision = 0)
-		=> precision is < 0 or > 4000
-			? throw new ArgumentOutOfRangeException(nameof(precision), "Must be between 0 to 4000.")
+	public static PropertyBuilder IsVarbinaryColumn(this PropertyBuilder builder, int precision = 0)
+		=> precision is < 0 or > 8000
+			? throw new ArgumentOutOfRangeException(nameof(precision), "Must be between 0 to 8000.")
 			: builder.HasColumnType(precision == 0 ? $"varbinary(max)" : $"varbinary({precision})");
 
 	/// <summary>
@@ -54,6 +83,6 @@ public static class PropertyBuilderExtensions
 	/// </summary>
 	/// <param name="builder">The builder for the property being configured.</param>
 	/// <returns>The same builder instance so that multiple calls can be chained.</returns>
-	public static PropertyBuilder HasXmlColumnType(this PropertyBuilder builder)
+	public static PropertyBuilder IsXmlColumn(this PropertyBuilder builder)
 		=> builder.HasColumnType("xml");
 }
