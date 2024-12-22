f92215ea27511589f2ca0d070a905e138e99ff3d
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-12-22 17:42:40 +0100

    fix: abstraction fixes applied

--- src/BB84.EntityFrameworkCore.Models.Abstractions/IAuditedModel.cs
+++ src/BB84.EntityFrameworkCore.Models.Abstractions/IAuditedModel.cs
@@ -6,25 +6,25 @@ namespace BB84.EntityFrameworkCore.Models.Abstractions;
 /// The interface for the audited models.
 /// </summary>
 /// <inheritdoc cref="IIdentity{TKey}"/>
-/// <inheritdoc cref="IUserAudited{TCreated, TModified}"/>
-public interface IAuditedModel<TKey, TCreated, TModified> : IIdentityModel<TKey>, IUserAudited<TCreated, TModified> where TKey : IEquatable<TKey>
+/// <inheritdoc cref="IUserAudited{TCreator, TEdited}"/>
+public interface IAuditedModel<TKey, TCreator, TEdited> : IIdentityModel<TKey>, IUserAudited<TCreator, TEdited> where TKey : IEquatable<TKey>
 { }
 
-/// <inheritdoc cref="IAuditedModel{TKey, TCreated, TModified}"/>
+/// <inheritdoc cref="IAuditedModel{TKey, TCreator, TEdited}"/>
 /// <remarks>
 /// The user auditing columns are of type <see cref="string"/>.
 /// </remarks>
 public interface IAuditedModel<TKey> : IAuditedModel<TKey, string, string?>, IUserAudited where TKey : IEquatable<TKey>
 { }
 
-/// <inheritdoc cref="IAuditedModel{TKey, TCreated, TModified}"/>
+/// <inheritdoc cref="IAuditedModel{TKey, TCreator, TEdited}"/>
 /// <remarks>
 /// The identity column is of type <see cref="Guid"/>.
 /// </remarks>
-public interface IAuditedModel<TCreated, TModified> : IAuditedModel<Guid, TCreated, TModified>, IIdentityModel
+public interface IAuditedModel<TCreator, TEdited> : IAuditedModel<Guid, TCreator, TEdited>, IIdentityModel
 { }
 
-/// <inheritdoc cref="IAuditedModel{TKey, TCreated, TModified}"/>
+/// <inheritdoc cref="IAuditedModel{TKey, TCreator, TEdited}"/>
 /// <remarks>
 /// The identity column is of type <see cref="Guid"/>.
 /// The user auditing columns are of type <see cref="string"/>.
--- src/BB84.EntityFrameworkCore.Models.Abstractions/IFullAuditedModel.cs
+++ src/BB84.EntityFrameworkCore.Models.Abstractions/IFullAuditedModel.cs
@@ -6,26 +6,26 @@ namespace BB84.EntityFrameworkCore.Models.Abstractions;
 /// The interface for full audited entities.
 /// </summary>
 /// <inheritdoc cref="IIdentity{TKey}"/>
-/// <inheritdoc cref="IUserAudited{TCreated, TModified}"/>
+/// <inheritdoc cref="IUserAudited{TCreator, TEdited}"/>
 /// <inheritdoc cref="ITimeAudited"/>
-public interface IFullAuditedModel<TKey, TCreated, TModified> : IIdentityModel<TKey>, IUserAudited<TCreated, TModified>, ITimeAudited where TKey : IEquatable<TKey>
+public interface IFullAuditedModel<TKey, TCreator, TEdited> : IIdentityModel<TKey>, IUserAudited<TCreator, TEdited>, ITimeAudited where TKey : IEquatable<TKey>
 { }
 
-/// <inheritdoc cref="IFullAuditedModel{TKey, TCreated, TModified}"/>
+/// <inheritdoc cref="IFullAuditedModel{TKey, TCreator, TEdited}"/>
 /// <remarks>
 /// The user auditing columns are of type <see cref="string"/>.
 /// </remarks>
 public interface IFullAuditedModel<TKey> : IFullAuditedModel<TKey, string, string?>, IUserAudited where TKey : IEquatable<TKey>
 { }
 
-/// <inheritdoc cref="IFullAuditedModel{TKey, TCreated, TModified}"/>
+/// <inheritdoc cref="IFullAuditedModel{TKey, TCreator, TEdited}"/>
 /// <remarks>
 /// The identity column is of type <see cref="Guid"/>.
 /// </remarks>
-public interface IFullAuditedModel<TCreated, TModified> : IFullAuditedModel<Guid, TCreated, TModified>, IIdentityModel
+public interface IFullAuditedModel<TCreator, TEdited> : IFullAuditedModel<Guid, TCreator, TEdited>, IIdentityModel
 { }
 
-/// <inheritdoc cref="IFullAuditedModel{TKey, TCreated, TModified}"/>
+/// <inheritdoc cref="IFullAuditedModel{TKey, TCreator, TEdited}"/>
 /// <remarks>
 /// The identity column is of type <see cref="Guid"/>.
 /// The user auditing columns are of type <see cref="string"/>.
